dfbc5b7dcec7d592d8f535c08467a387fa42bb0e
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2023-04-26 10:49:28 +0200

    populating build

---
+++ lib/libpng/libpng16-relwithdebinfo.cmake
@@ -0,0 +1,31 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "png" for configuration "RelWithDebInfo"
+set_property(TARGET png APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(png PROPERTIES
+  IMPORTED_IMPLIB_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libpng16.lib"
+  IMPORTED_LINK_INTERFACE_LIBRARIES_RELWITHDEBINFO "C:/gtk-build/gtk/x64/release/lib/zlib1.lib"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/bin/libpng16.dll"
+  )
+
+list(APPEND _cmake_import_check_targets png )
+list(APPEND _cmake_import_check_files_for_png "${_IMPORT_PREFIX}/lib/libpng16.lib" "${_IMPORT_PREFIX}/bin/libpng16.dll" )
+
+# Import target "png_static" for configuration "RelWithDebInfo"
+set_property(TARGET png_static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(png_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C;RC"
+  IMPORTED_LINK_INTERFACE_LIBRARIES_RELWITHDEBINFO "C:/gtk-build/gtk/x64/release/lib/zlib1.lib"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libpng16_static.lib"
+  )
+
+list(APPEND _cmake_import_check_targets png_static )
+list(APPEND _cmake_import_check_files_for_png_static "${_IMPORT_PREFIX}/lib/libpng16_static.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
